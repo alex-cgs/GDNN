9b5daee00d7f93327f6763e4bbe8c9328dc56de9
Tinouert Alexandre <alexandretinouert@gmail.com>
2024-03-25 15:07:13 +0100

    Extracting user infos

--- bsps4.py
+++ bsps4.py
@@ -1,15 +1,29 @@
-import numpy as np
 import cv2
-import pyautogui
-import time
+import pytesseract
+from PIL import ImageGrab
+import numpy as np
 
-# take screenshot using pyautogui
-start_time = time.time()  # Record start time
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+screen_region = (166, 76, 368, 957)  # Example: (left, top, width, height)
+
 i = 0
-while i < 1:
-    image = pyautogui.screenshot(f"image{i}.png")
-    i += 1
-end_time = time.time()  # Record end time
 
-execution_time = end_time - start_time
-print(f"Execution time: {execution_time} seconds")
+while i < 10:
+    screenshot = ImageGrab.grab(bbox=screen_region)
+
+    screenshot_np = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
+
+    text = pytesseract.image_to_string(screenshot_np)
+
+    print("Extracted Text:", text)
+
+    # Display the screenshot
+    cv2.imshow('Screenshot', screenshot_np)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+    i += 1
+
+cv2.destroyAllWindows()
